f350872dba3fd88f249553a9814da6fc22ce1759
integraldx <integraldx@integraldx.me>
2023-07-17 00:30:31 +0900

    Install codedeploy agent for misskey instance

--- scripts/misskey_user_data.sh
+++ scripts/misskey_user_data.sh
@@ -3,3 +3,12 @@ sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+
+sudo yum install -y ruby
+sudo yum install -y wget
+
+cd /home/ec2-user
+wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
