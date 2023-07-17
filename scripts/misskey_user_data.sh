d75ec92fbff774402a8f9dcc40fe9df7e32449b7
integraldx <integraldx@integraldx.me>
2023-07-17 20:20:02 +0900

    symlink docker-compose executable to path

--- scripts/misskey_user_data.sh
+++ scripts/misskey_user_data.sh
@@ -11,6 +11,7 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 
 sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 cd /home/ec2-user
 wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
