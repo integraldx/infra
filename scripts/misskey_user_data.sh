3549138104352133dacb1a96836f26f9341cca1e
integraldx <integraldx@integraldx.me>
2023-07-17 20:11:30 +0900

    Fix docker-compose install command

--- scripts/misskey_user_data.sh
+++ scripts/misskey_user_data.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 sudo yum update -y
-sudo yum install -y docker
-sudo yum install -y docker-compose
+sudo amazon-linux-extras install docker
 sudo yum install -y ruby
 sudo yum install -y wget
 sudo yum install -y ec2-instance-connect
 
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+
+sudo chmod +x /usr/local/bin/docker-compose
 
 cd /home/ec2-user
 wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
